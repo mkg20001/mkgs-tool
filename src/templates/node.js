c79006839b6945eddf9dbefbc368603bcd9ed3f3
Maciej Krüger <mkg20001@gmail.com>
2019-11-23 06:40:20 +0100

    feat: noNodeLock/noYarnLock

--- src/templates/node.js
+++ src/templates/node.js
@@ -2,11 +2,11 @@
 
 module.exports = (config) => {
   return {
-    gitignore: [
+    gitignore: [ // TODO: possibly consider renaming it from no to allow?
       'node_modules',
       !config.noYarn && 'yarn-error.log',
-      !config.noLock && 'package-lock.json',
-      !config.noLock && !config.noYarn && 'yarn.lock'
+      !config.noLock && !config.noNodeLock && 'package-lock.json',
+      !config.noLock && !config.noYarnLock && !config.noYarn && 'yarn.lock'
     ],
     packageJSON: config.noTools ? {} : {
       scripts: {
