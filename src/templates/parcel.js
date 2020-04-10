a8946993b7714329737fc1cd751d3c10737d779a
Maciej Krüger <mkg20001@gmail.com>
2020-04-10 23:45:21 +0200

    feat: add rmrf dist

--- src/templates/parcel.js
+++ src/templates/parcel.js
@@ -7,7 +7,7 @@ module.exports = (config) => {
     packageJSON: {
       scripts: config.noScripts ? {} : {
         start: `parcel start ${config.entry} ${config.startFlags || ''} ${config.flags || ''}`,
-        build: `parcel build ${config.entry} ${config.buildFlags || ''} ${config.flags || ''}`
+        build: `rm -rf dist && parcel build ${config.entry} ${config.buildFlags || ''} ${config.flags || ''}`
       }
     }
   }
