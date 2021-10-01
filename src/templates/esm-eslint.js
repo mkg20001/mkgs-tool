a3fef5f9f76d3fc09dad6d1766baf1dbfae259dc
Maciej Krüger <mkg20001@gmail.com>
2021-10-01 05:32:41 +0200

    fix(esmEslint): disable prefer node protocol

--- src/templates/esm-eslint.js
+++ src/templates/esm-eslint.js
@@ -13,7 +13,9 @@ module.exports = config => {
       rules: {
         'node/no-unsupported-features/es-syntax': 'off',
         // directory importing is not supported, keep this off for now (it wants to modify thing/index.js into thing, which is dir import)
-        'unicorn/import-index': 'off'
+        'unicorn/import-index': 'off',
+        // this doesn't properly work yet but wants to be applied
+        'unicorn/prefer-node-protocol': 'off'
       }
     }
   }
